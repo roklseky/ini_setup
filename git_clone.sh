f2bbbc4a9db58030dc3633bee1e83e7d4282d73b
roman <roklseky@gmail.com>
2020-07-28 15:11:09 +0200

    < /dev/tty

--- git_clone.sh
+++ git_clone.sh
@@ -4,7 +4,7 @@ VENDOR=$(lscpu | grep 'Vendor ID' | awk '{print $3}')
 if [ "$VENDOR" = "GenuineIntel" ]
 
 then
-read -p "Add yourself to SUDO sudo usermod -aG sudo '$(whoami)'"
+read -p "Add yourself to SUDO sudo usermod -aG sudo '$(whoami)'" < /dev/tty
 fi
 
 PTH=`eval echo ~$USER`
@@ -12,7 +12,7 @@ HST=`hostname`
 ssh-keygen -t ecdsa -b 521 -f $PTH/.ssh/id_$HST
 cat $PTH/.ssh/id_$HST.pub
 
-read -p "Add pub to github https://github.com/settings/keys for SSH git clone"
+read -p "Add pub to github https://github.com/settings/keys for SSH git clone" < /dev/tty
 
 eval $(ssh-agent)
 ssh-add $PTH/.ssh/id_$HST
