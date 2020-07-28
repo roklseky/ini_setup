586b9685d8b5678ef8a22e528a47d519b6877930
roman <roklseky@gmail.com>
2020-07-28 12:23:27 +0200

    $PTH

--- git_clone.sh
+++ git_clone.sh
@@ -17,7 +17,7 @@ read -p "Add pub to github https://github.com/settings/keys for SSH git clone"
 eval $(ssh-agent)
 ssh-add $PTH/.ssh/id_$HST
 git clone git@github.com:roklseky/dockerfile.git
-mv dockerfile/`hostname` docker
+mv dockerfile/`hostname` $PTH/docker
 rm -rf dockerfile
 
 
