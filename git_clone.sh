c6dd6d692470e8f53df3c0bf4fcf91f533878317
roman <roklseky@gmail.com>
2020-07-28 22:41:44 +0200

    rm pub

--- git_clone.sh
+++ git_clone.sh
@@ -32,7 +32,7 @@ echo IdentitiesOnly yes >> $PTH/.ssh/config
 
 #git clone git@github.com:roklseky/dockerfile.git
 echo "ssh-agent bash -c \
-'ssh-add $PTH/.ssh/id_$HST.pub; git clone git@github.com:user/dockerfile.git'" \
+'ssh-add $PTH/.ssh/id_$HST; git clone git@github.com:user/dockerfile.git'" \
 >$PTH/gitclone.sh
 
 sh $PTH/gitclone.sh
