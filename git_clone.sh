30dc8bb4810bbf14b16a433947596c13240eb48b
roman <roklseky@gmail.com>
2020-07-28 17:18:05 +0200

    sudo

--- git_clone.sh
+++ git_clone.sh
@@ -3,7 +3,9 @@
 VENDOR=$(lscpu | grep 'Vendor ID' | awk '{print $3}')
 if [ "$VENDOR" = "GenuineIntel" ] 
 then
-sudo usermod -aG sudo "$(whoami)"
+PTH=`eval echo ~$USER`
+USR=$( echo ${PTH##/*/} )
+sudo usermod -aG sudo $USR
 fi
 
 PTH=`eval echo ~$USER`
