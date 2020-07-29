306ca5d9a7706dd16955042b5fa51ae622b93d11
roman <roklseky@gmail.com>
2020-07-29 17:55:54 +0200

    tune

--- git_clone.sh
+++ git_clone.sh
@@ -4,42 +4,42 @@
 VENDOR=$(lscpu | grep 'Vendor ID' | awk '{print $3}')
 if [ "$VENDOR" = "GenuineIntel" ] 
 then
+DVC=NUC
 PTH=`eval echo ~$USER`
 USR=$( echo ${PTH##/*/} )
 sudo usermod -aG sudo $USR
 fi
-
+DVC=pi
 PTH=`eval echo ~$USER`
 HST=`hostname`
 mkdir $PTH/.ssh
 USR=$( echo ${PTH##/*/} )
+
+# Generate SSH keys
 ssh-keygen -t ecdsa -b 521 -f $PTH/.ssh/id_$HST
 chown -R $USR:$USR $PTH/.ssh
 cat $PTH/.ssh/id_$HST.pub
 
+# Add pub key to git
 echo "$(tput setaf 1)$(tput setab 7)\
 "Add pub key to github "->" https://github.com/settings/keys for SSH git clone. \
 Press [ENTER] when done"$(tput sgr 0)"
 read continue
 echo yes
 
-#echo Host github > $PTH/.ssh/config
-#echo HostName github.com >> $PTH/.ssh/config
-#echo IdentityFile $PTH/.ssh/id_$HST >> $PTH/.ssh/config
-#echo IdentitiesOnly yes >> $PTH/.ssh/config
-
-#echo Host wdmycloud >> $PTH/.ssh/config
-#echo HostName 10.0.0.122 >> $PTH/.ssh/config
-#echo IdentityFile $PTH/.ssh/id_$HST >> $PTH/.ssh/config
-#echo IdentitiesOnly yes >> $PTH/.ssh/config
-
-#git clone git@github.com:roklseky/dockerfile.git
+# Clone personal git repo
 eval $(ssh-agent)
 ssh-add $PTH/.ssh/id_$HST; 
 git clone git@github.com:roklseky/dockerfile.git
 
-PTH=`eval echo ~$USER`
-mv dockerfile/`hostname` $PTH/docker
-rm -rf dockerfile
-
-sh $PTH/docker/ini_install.sh
+#Discover if pi of NUC
+mv $PTH/dockerfile/ini_install_NUC.sh $PTH/ini_install_NUC.sh
+mv $PTH/dockerfile/ini_install_pi.sh $PTH/ini_install_pi.sh
+mv $PTH/dockerfile/ini_reboot_install_pi.sh $PTH/dockerfile/ini_reboot_install_pi.sh
+mv $PTH/dockerfile/pisn.txt $PTH/pisn.txt
+if [ "$DVC" = "NUC" ] 
+then
+sh $PTH/ini_install_NUC.sh
+else
+sh $PTH/ini_install_pi.sh
+fi
